fd9047176206f8d00cfc48edf63cbbb95b48340e
Rod Cloutier <rodcloutier@gmail.com>
2016-12-20 09:26:11 -0500

    Misc fixes

--- build.sh
+++ build.sh
@@ -1,10 +1,24 @@
 #! /bin/bash
 
-cp media/flavescens.jpg html/bg.jpg
-docker build -t rodcloutie/zebrasoma:flavescens .
+function build {
 
-cp media/xanthurum.jpg html/bg.jpg
-docker build -t rodcloutie/zebrasoma:xanthurum .
+    cp media/$1.jpg html/bg.jpg
+    sed -i '' "s/specie/$1/" html/index.html
+    docker build -t rodcloutie/zebrasoma:$1 .
+    sed -i '' "s/$1/specie/" html/index.html
+}
 
-cp media/zebrus.jpg html/bg.jpg
-docker build -t rodcloutie/zebrasoma:zebrus .
+build flavescens
+build xanthurum
+build zebrus
+
+# cp media/flavescens.jpg html/bg.jpg
+# sed -i 's/specie/flavescens/' html/index.html
+# docker build -t rodcloutie/zebrasoma:flavescens .
+# sed -i 's/flavescens/specie' html/index.html
+
+# cp media/xanthurum.jpg html/bg.jpg
+# docker build -t rodcloutie/zebrasoma:xanthurum .
+
+# cp media/zebrus.jpg html/bg.jpg
+# docker build -t rodcloutie/zebrasoma:zebrus .
